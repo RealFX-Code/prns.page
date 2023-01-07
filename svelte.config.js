fca624e757276324b90a09668ed6970847b7e5b8
rlfx <mathias.aronsen@icloud.com>
2023-01-07 15:48:11 +0100

    add CF support

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
 	}
 };
 
