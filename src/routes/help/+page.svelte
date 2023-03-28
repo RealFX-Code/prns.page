ef33b875959a5dac662d39bbf771ea2458b37ca1
rlfx <rlfx@realfx.rocks>
2023-03-28 15:42:39 +0200

    add help for language

--- src/routes/help/+page.svelte
+++ src/routes/help/+page.svelte
@@ -3,6 +3,8 @@
     // route: " /help "
     // file : " help/+page.svelte "
 
+    import { page } from '$app/stores';
+
 </script>
 
 <main>
@@ -16,16 +18,23 @@
                 "username" with the users username.
             </p>
             <code>
-                {window.location.protocol}//{window.location.host}/@username
+                https://prns.realfx.rocks/@username
             </code>
         </div>
         <div class="section usage">
             <h3>Get a user's profile view from the username input:</h3>
             <p>
                 Simply write the user's username in the username field in the navbar, then hit enter.
-
-                NOTE: Don't prepend an @ to the username!
             </p>
+        </div>
+        <div class="section lang">
+            <h3>Set the language for a page:</h3>
+            <p>
+                Add the "lang" search parameter along with your request. So to get the profile of "@username", with the language "es" ( spanish ) you'd have to go to:
+            </p>
+            <code>
+                https://prns.realfx.rocks/@username?lang=es
+            </code>
         </div>
     </div>
 </main>
